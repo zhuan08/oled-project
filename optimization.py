78ce2552812c87181de1f4535b5d464f78b9ae22
zhuan08 <zhuang1356@gmail.com>
2025-06-27 16:54:40 -0400

    Updated script to include error messages

--- optimization.py
+++ optimization.py
@@ -17,9 +17,8 @@ try:
 except FileExistsError:
     pass
 
-mol_id = []
+mol_ids = []
 path_id = []
-final_mol_id = []
 error_msg = []
 diff_energy_list = []
 
@@ -29,25 +28,22 @@ for mol in glob.glob('structures_mol2/*.mol2'):
         updated_file = mol.replace('structures_mol2', '')
         updated_file = updated_file.replace('.mol2', '')
         updated_file = updated_file.replace('/', '')
-        mol_id.append(updated_file)
-print(mol_id)
+        mol_ids.append(updated_file)
 
 # Creating list of paths for mol2 molecules
 for path in glob.glob('structures_mol2/*.mol2'):
     if '.mol2' in path:
         path_id.append(path)
-print(path_id)
 
-for mol_id, path_id in zip(mol_id, path_id):
+for mol_id, path_id in zip(mol_ids, path_id):
     geom_path = os.path.join(geom_dir_name, f'{mol_id}.xyz')
     if os.path.exists(geom_path):
         atom = ase.io.read(filename=geom_path)
-        final_mol_id.append(mol_id)
+        error_msg.append('No Error Message')
     else:
         molecule = Chem.MolFromMol2File(path_id, removeHs=False, sanitize=False)
         if molecule is None:
             raise ValueError(f'MolFromMol2File returned None on {mol_id}.mol2')
-        molecule = Chem.AddHs(molecule)
         try:
             octahedral_embed(molecule, isomer="tridentate")
         except Exception as e: 
@@ -73,3 +69,8 @@ for mol_id, path_id in zip(mol_id, path_id):
             error_msg.append(e)
             continue
         ase.io.write(filename=geom_path, images=atom)
+    error_msg.append('No Error Message')
+
+i = 0
+for i in range(len(mol_ids)):
+    print(mol_ids[i], error_msg[i])
